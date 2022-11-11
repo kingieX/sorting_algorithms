6328c48a1dfe4e19a17d8cbc892a5f92a657c9ed
Nnachi Chima Kingsley <chimakingsley216@gmail.com>
2022-11-11 05:37:16 -0800

    modify insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,9 +1,10 @@
 #include "sort.h"
+
 /**
- * swap_nodes - function that swap two nodes
- * @h: Pointer to the head of the list
- * @n1: Pointer to the first node to be swapped
- * @n2: Pointer to the second node to be swapped
+ * swap_nodes - Swap two nodes in a listint_t doubly-linked list.
+ * @h: A pointer to the head of the doubly-linked list.
+ * @n1: A pointer to the first node to swap.
+ * @n2: The second node to swap.
  */
 void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
 {
@@ -12,7 +13,7 @@ void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
 		n2->next->prev = *n1;
 	n2->prev = (*n1)->prev;
 	n2->next = *n1;
-	if ((*n1)->prev  != NULL)
+	if ((*n1)->prev != NULL)
 		(*n1)->prev->next = n2;
 	else
 		*h = n2;
@@ -21,25 +22,28 @@ void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
 }
 
 /**
- * insertion_sort_list - function that sorts a doubly linked list
- * @list: List of integers
+ * insertion_sort_list - Sorts a doubly linked list of integers
+ *                       using the insertion sort algorithm.
+ * @list: A pointer to the head of a doubly-linked list of integers.
  *
+ * Description: Prints the list after each swap.
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *iteration, *insert, *tmp;
+	listint_t *iter, *insert, *tmp;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	for (iteration = (*list)->next; iteration != NULL; iteration = tmp)
+	for (iter = (*list)->next; iter != NULL; iter = tmp)
 	{
-		tmp = iteration->next;
-		insert = iteraton->prev;
-		while (insert != NULL && iteration->n < insert->n)
+		tmp = iter->next;
+		insert = iter->prev;
+		while (insert != NULL && iter->n < insert->n)
 		{
-			swap_nodes(list, &insert, iteration);
+			swap_nodes(list, &insert, iter);
 			print_list((const listint_t *)*list);
 		}
 	}
 }
+
